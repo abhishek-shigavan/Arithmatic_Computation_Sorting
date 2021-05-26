aa2591528f808b440f5ed2d10147bc6f1276a972
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-05-26 18:58:30 +0530

    Add: Computing Equation 3

--- arithmaticComputationSorting.sh
+++ arithmaticComputationSorting.sh
@@ -7,3 +7,6 @@ read -p " Enter 3rd Number " c
 arithmaticOperation1=`awk "BEGIN {print $a + $b * $c}"`
 arithmaticOperation2=`awk "BEGIN {print $a * $b + $c}"`
 
+arithmaticOperation3=`awk "BEGIN {print $c + $a / $b}"`
+arithmaticOperation3=${arithmaticOperation3%.*}
+
