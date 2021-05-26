8d2c05546a5db73995ba37cc26e5fc71c7a6af68
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-05-26 19:11:12 +0530

    Add: Storing arithmatic operations result

--- arithmaticComputationSorting.sh
+++ arithmaticComputationSorting.sh
@@ -11,3 +11,8 @@ arithmaticOperation3=`awk "BEGIN {print $c + $a / $b}"`
 arithmaticOperation3=${arithmaticOperation3%.*}
 
 arithmaticOperation4=`awk "BEGIN {print $a % $b + $c}"`
+
+arithmaticOperation_dictonary["a+b*c"]="$arithmaticOperation1"
+arithmaticOperation_dictonary["a*b+c"]="$arithmaticOperation2"
+arithmaticOperation_dictonary["c+a/b"]="$arithmaticOperation3"
+arithmaticOperation_dictonary["a%b+c"]="$arithmaticOperation4"
